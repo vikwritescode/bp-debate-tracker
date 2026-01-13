935332241c13846ab9370478e12e52f412bee07d
vik <foo@bar.com>
2026-01-13 00:43:58 +0000

    remove unnecessary code

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,7 +1,1 @@
-from .get_positions import get_positions
-from .get_results import get_results
-from .get_speaker import get_speaker
-from .get_speaks import get_speaks
-from .generate_records import generate_records
-from .get_slugs import get_slugs
 from .get_data import get_data
