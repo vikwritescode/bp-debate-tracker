363e2242e6e96dbc85682b9513a69e4c53249447
vik <foo@bar.com>
2026-01-26 16:56:19 +0000

    get rounds simultaneously

--- src/utils/get_data.py
+++ src/utils/get_data.py
@@ -1,19 +1,56 @@
 import requests
+import asyncio
+import aiohttp
 
 def get_data(tab_url: str, slug: str, speaker_url: str):
-    # get the speaker object
+    """
+    Make API requests, getting the data for a speaker at a tournament
+    
+    :param tab_url: the tab URL
+    :type tab_url: str
+    :param slug: the tournament slug
+    :type slug: str
+    :param speaker_url: the speaker URL
+    :type speaker_url: str
+    """
+    
+    async def fetch(session, url):
+        async with session.get(url) as resp:
+            resp.raise_for_status()
+            return await resp.json()
+    
+    async def get_pairings(urls):
+        async with aiohttp.ClientSession() as session:
+            tasks = {u: fetch(session, u) for u in urls}
+            results = await asyncio.gather(*tasks.values())
+            data_map = dict(zip(tasks.keys(), results))
+            return data_map
+    
+    
+    
     s_r = requests.get(speaker_url)
     s = s_r.json()
     
     team_url = s["team"]
     
+    speak_standings_response = requests.get(f"{tab_url}/api/v1/tournaments/{slug}/speakers/standings/rounds")
+    speak_standings = speak_standings_response.json()
+    
     resp = requests.get(f"{tab_url}/api/v1/tournaments/{slug}/teams/standings/rounds")
     round_stands = resp.json()
+    
     print("(1) gotten round standings")
     
     results = dict()
     # get our specific entry in round_stands
     team_round_wise = next(team for team in round_stands if team["team"] == team_url)
+    
+    # fetch all rounds concurrently
+    pairings_urls = [r["round"] for r in team_round_wise["rounds"]]
+    pairings_jsons = asyncio.run(get_pairings(pairings_urls))
+    
+    
+    # processing for each round
     for round in team_round_wise["rounds"]:
         if round["score"] is None:
             # ignore outrounds
@@ -25,9 +62,8 @@ def get_data(tab_url: str, slug: str, speaker_url: str):
         result["motion"] = "blank..?"
         result["speaks"] = 0 # default, if not replaced by round
         
-        # getting data from round URL
-        round_request_response = requests.get(round["round"])
-        round_data = round_request_response.json()
+        # getting data from round URL (already fetched tho)
+        round_data = pairings_jsons[round["round"]]
         print("(2) gotten round data")
         
         round_motion_set = round_data["motions"]
@@ -48,8 +84,6 @@ def get_data(tab_url: str, slug: str, speaker_url: str):
         results[round["round"]] = result
     
     # get speaks and append to our results
-    speak_standings_response = requests.get(f"{tab_url}/api/v1/tournaments/{slug}/speakers/standings/rounds")
-    speak_standings = speak_standings_response.json()
     print("(4) gotten speaks data")
     # get our speaker
     relevant = next((speaker["rounds"] for speaker in speak_standings if speaker["speaker"] == speaker_url), [])
