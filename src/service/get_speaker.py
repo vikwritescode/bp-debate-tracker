fa8eeb4df66599190f53033451843a57f9b93ae1
vik <163123415+vikwritescode@users.noreply.github.com>
2026-02-06 01:39:43 +0000

    raise for status

--- src/service/get_speaker.py
+++ src/service/get_speaker.py
@@ -19,12 +19,11 @@ def get_speaker(url :str, slug: str, name: str) -> list:
         print(url)
         print(link)
         response = requests.get(f"{link}/api/v1/tournaments/{slug}/speakers")
+        response.raise_for_status()
     except requests.exceptions.RequestException as e:
         if e.response.status_code == 401:
             raise TabAuthError
         raise ValueError("Could not make a request to URL. Double check.")
-    if response.status_code != 200:
-        raise RuntimeError(f"[{response.status_code}] unwanted response: {response.reason}")
     data = response.json()
     # filter out unnecessary entries
     relevant = [{"name": entry["name"], "team": entry["team"], "url": entry["url"]} 
